1d4a278b384a6c1c4b20d2f0e149ce0e0a25aeae
Danilo Amorim <daniloamorim@MacBook-Pro-de-Danilo.local>
2024-01-30 09:42:30 -0300

    adicionando JWT

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,8 @@ const User = require('../models/User')
 
 const bcrypt = require('bcrypt')
 
+const createUserToken = require('../helpers/create-user-token')
+
 module.exports = class UserController {
     static async register(req, res) {
         const { name, email, phone, password, confirmpassword } = req.body
@@ -54,13 +56,9 @@ module.exports = class UserController {
 
         try {
             const newUser = await user.save()
-            res.status(201).json({
-                message: 'Usuario Criado',
-                newUser
-            })
-        } catch (error) {
+            await createUserToken(newUser,req,res)
+            } catch (error) {
             res.status(500).json({ mensagem: error })
-
         }
     }
 }
---
+++ backend/helpers/create-user-token.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken") //  O jsonwebtoken é uma biblioteca bastante utilizada para a geração e verificação de JSON Web Tokens (JWT).
+
+const createUserToken = async (user, req, res) => {
+
+    // create a token
+    const token = jwt.sign({
+        name: user.name,
+        id: user._id
+    }, "nossosecret") // O método sign da biblioteca jsonwebtoken é usado para criar um token JWT.O primeiro argumento é um objeto que representa o conteúdo do token. Neste caso, parece incluir o nome do usuário (user.name) e o ID do usuário (user._id). O segundo argumento é a chave secreta usada para assinar o token. Neste caso, a string "nossosecret" está sendo usada como chave secreta. A chave secreta é usada para garantir que o token não tenha sido adulterado.
+
+
+    // return token
+
+    res.status(200).json({
+        message: "Você está autenticado",
+        token: token,
+        userId: user._id,
+    }) // Após a criação do token, o servidor envia uma resposta HTTP com status 200 (OK) e um objeto JSON no corpo da resposta.
+}
+module.exports = createUserToken
+
+// helpers fornecem funcionalidades auxiliares para tornar o desenvolvimento mais fácil, eficiente e organizado. 
+// Os JWTs são uma forma de representar informações entre duas partes de uma forma que pode ser verificada e confiável. 
